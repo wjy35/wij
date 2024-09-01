e8a8ae0e8d04408840e3307471367ceab3592713
왕준영 <wangsun7@naver.com>
2024-09-02 07:32:26 +0900

    feat: Add Icon & .svg Files

---
+++ src/main/java/com/wjy35/wij/ui/icon/Icons.java
@@ -0,0 +1,11 @@
+package com.wjy35.wij.ui.icon;
+
+import com.intellij.openapi.util.IconLoader;
+import javax.swing.*;
+
+public class Icons {
+    public static final Icon UPDATE_AND_JUDGE = IconLoader.getIcon("/icons/ConcurrencyDiagramToolwindow.svg", Icons.class);
+    public static final Icon JUDGE = IconLoader.getIcon("/icons/run.svg", Icons.class);
+    public static final Icon RUN = IconLoader.getIcon("/icons/stubsFile.svg",Icons.class);
+    public static final Icon DELETE = IconLoader.getIcon("/icons/delete.svg",Icons.class);
+}
