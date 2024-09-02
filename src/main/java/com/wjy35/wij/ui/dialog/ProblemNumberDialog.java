5a99671e4f889c18d779f8446ad05848eefd90b2
왕준영 <wangsun7@naver.com>
2024-09-02 10:12:26 +0900

    feat: Add ProblemNumberDialog

---
+++ src/main/java/com/wjy35/wij/ui/dialog/ProblemNumberDialog.java
@@ -0,0 +1,46 @@
+package com.wjy35.wij.ui.dialog;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.ui.InputValidator;
+import com.intellij.openapi.ui.Messages;
+import com.wjy35.wij.ui.icon.Icons;
+import java.util.concurrent.atomic.AtomicReference;
+
+
+public class ProblemNumberDialog {
+    public static String showAndGet(){
+        AtomicReference<String> problemNumber = new AtomicReference<>();
+
+        ApplicationManager.getApplication().invokeAndWait(() -> {
+            problemNumber.set(Messages.showInputDialog(
+                    "문제 번호를 입력하세요 (예: 1000, 12325)",
+                    "WangJun Intellij Judge",
+                    Icons.RUN,
+                    "",
+                    validator
+            ));
+        });
+
+        return problemNumber.get();
+    }
+
+    private static final InputValidator validator = new InputValidator() {
+        @Override
+        public boolean checkInput(String inputString) {
+            if(inputString==null) return false;
+            if(inputString.trim().isEmpty()) return false;
+
+            try{
+                Integer.parseInt(inputString);
+            }catch (NumberFormatException e){
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public boolean canClose(String inputString) {
+            return checkInput(inputString);
+        }
+    };
+}
